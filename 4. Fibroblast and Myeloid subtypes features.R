9c758c6cdc25c9c3599228f594932b1367e6ba98
hedyBao <52365869+hedyBao@users.noreply.github.com>
2024-03-05 12:17:04 +0800

    4.Fibroblast and Myeloid subtypes features.R

--- 4. Fibroblast and Myeloid subtypes features.R
+++ 4. Fibroblast and Myeloid subtypes features.R
@@ -1,15 +1,4 @@
-.cluster_cols <- c(
-  "#DC050C", "#FB8072", "#1965B0", "#7BAFDE", "#882E72",
-  "#B17BA6", "#FF7F00", "#FDB462", "#E7298A", "#E78AC3",
-  "#33A02C", "#B2DF8A", "#55A1B1", "#8DD3C7", "#A6761D",
-  "#E6AB02", "#7570B3", "#BEAED4", "#666666", "#999999",
-  "#aa8282", "#d4b7b7", "#8600bf", "#ba5ce3", "#808000",
-  "#aeae5c", "#1e90ff", "#00bfff", "#56ff0d", "#ffff00")
 
-OutPath <- "/work/brj/Collaboration/2022/scRNA/HNSCC/Revise3/"
-folder <- "Fig3"
-
-##fibro cell=======
 HNSCC_fibro <- readRDS("/work/yye/Project/Collaboration/HNSC/Stroma/HNSC_Fibro1_DefineTypes.rds.gz")
 Idents(HNSCC_fibro) <- HNSCC_fibro$DefineTypes
 
